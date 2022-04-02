206792c22b075ddc1e3559300b643dcf9b830808
Ramiro Fernandez <ramiro.f2812gmail.com>
2022-04-02 12:35:37 -0300

    fetch

--- js/archivo.js
+++ js/archivo.js
@@ -1,5 +1,4 @@
-// uso del Spread
-productos = [...tortas, ...galletas, ...combos];
+const misProd = "js/data/data.json";
 
 let carrito = [];
 if (localStorage.getItem("Carrito")) {
@@ -11,12 +10,14 @@ if (localStorage.getItem("costo")) {
   costoTotal = JSON.parse(localStorage.getItem("costo"));
 }
 
-// Muestra los productos en el HTML
-productos.forEach((producto) => {
-  const contenedor = document.getElementById("flex2");
-  let productView = document.createElement("div");
-  // tarjetas
-  productView.innerHTML = ` 
+fetch(misProd)
+  .then((respuesta) => respuesta.json())
+  .then((data) => {
+    data.forEach((producto) => {
+      const contenedor = document.getElementById("flex2");
+      let productView = document.createElement("div");
+      // tarjetas
+      productView.innerHTML = ` 
   <div class="card producto" style="width: 18rem">
   <img
        src="${producto.img}"
@@ -31,45 +32,46 @@ productos.forEach((producto) => {
            </div>
        </div>
        `;
-  contenedor.append(productView);
+      contenedor.append(productView);
 
-  // Verifica si ya en el carrito existe el producto agregado
-  const botonAgregar = document.getElementById(`btn${producto.id}`);
-  botonAgregar.onclick = () => {
-    const existe = carrito.find((elemento) => elemento.id === producto.id);
+      // Verifica si ya en el carrito existe el producto agregado
+      const botonAgregar = document.getElementById(`btn${producto.id}`);
+      botonAgregar.onclick = () => {
+        const existe = carrito.find((elemento) => elemento.id === producto.id);
 
-    existe
-      ? carrito.forEach((elemento) => {
-          if (elemento.id === producto.id) {
-            elemento.cantidad++;
-          }
-        })
-      : carrito.push({
-          name: producto.name,
-          price: producto.price,
-          id: producto.id,
-          img: producto.img,
-          cantidad: 1,
-        });
+        existe
+          ? carrito.forEach((elemento) => {
+              if (elemento.id === producto.id) {
+                elemento.cantidad++;
+              }
+            })
+          : carrito.push({
+              name: producto.name,
+              price: producto.price,
+              id: producto.id,
+              img: producto.img,
+              cantidad: 1,
+            });
 
-    costoTotal = costoTotal + producto.price;
+        costoTotal = costoTotal + producto.price;
 
-    console.log(carrito, costoTotal);
+        console.log(carrito, costoTotal);
 
-    localStorage.setItem("Carrito", JSON.stringify(carrito));
-    localStorage.setItem("costo", costoTotal);
+        localStorage.setItem("Carrito", JSON.stringify(carrito));
+        localStorage.setItem("costo", costoTotal);
 
-    // Toastify
-    Toastify({
-      text: "Se ha agregado tu producto",
-      duration: 2000,
-      position: "left",
-      style: {
-        background: "#bd5c56",
-      },
-    }).showToast();
-  };
-});
+        // Toastify
+        Toastify({
+          text: "Se ha agregado tu producto",
+          duration: 2000,
+          position: "left",
+          style: {
+            background: "#bd5c56",
+          },
+        }).showToast();
+      };
+    });
+  });
 
 /*
   Elegi usar el Toastify para poder avisarle al usuario las acciones que va realizando sin interrumpir su interaccion con la pagina.
